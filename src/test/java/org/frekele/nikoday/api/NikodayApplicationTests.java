32757ad4a12fdef6813abd2e46df73d8dad521af
Leandro Kersting de Freitas <leandro@frekele.org>
2020-11-22 23:55:36 -0300

    .

---
+++ src/test/java/org/frekele/nikoday/api/NikodayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.frekele.nikoday.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
