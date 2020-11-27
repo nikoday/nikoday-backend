45cf38a9db4dca35fae1c519ed680bdc8c1fbbf5
Leandro Kersting de Freitas <leandro@frekele.org>
2020-11-27 03:27:01 -0300

    EnableMongoAuditing

--- src/main/java/org/frekele/nikoday/NikodayApplication.java
+++ src/main/java/org/frekele/nikoday/NikodayApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoAuditing
 @SpringBootApplication(scanBasePackages = "org.frekele.nikoday",//
         exclude = {
                 DataSourceAutoConfiguration.class,
